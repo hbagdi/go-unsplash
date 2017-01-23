b6287026fc8ae7e2a6c067081c47d981eb9b646e
Hardik Bagdi <hbagdi1@binghamton.edu>
2017-01-22 23:01:45 -0500

    TODO added;search service structs

---
+++ search_results.go
@@ -0,0 +1,47 @@
+// Copyright (c) 2017 Hardik Bagdi <hbagdi1@binghamton.edu>
+//
+// MIT License
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+package unsplash
+
+// UserSearchResult represnts the result for a search for users.
+type UserSearchResult struct {
+	Total      *int    `json:"total"`
+	TotalPages *int    `json:"total_pages"`
+	Results    *[]User `json:"results"`
+}
+
+// PhotoSearchResult represnts the result for a search for photos.
+type PhotoSearchResult struct {
+	Total      *int     `json:"total"`
+	TotalPages *int     `json:"total_pages"`
+	Results    *[]Photo `json:"results"`
+}
+
+// CollectionSearchResult represnts the result for a search for collections.
+type CollectionSearchResult struct {
+	Total      *int          `json:"total"`
+	TotalPages *int          `json:"total_pages"`
+	Results    *[]Collection `json:"results"`
+}
+
+//TODO add unexported struct for query, page and per_page
